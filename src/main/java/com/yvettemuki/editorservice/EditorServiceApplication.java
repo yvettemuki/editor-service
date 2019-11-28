c68a087ea219c87399e8f09b14867cbb08c82f9e
Shuqi Liao <373628977@qq.com>
2019-11-28 18:32:46 +0800

    finish send picXML to service

--- src/main/java/com/yvettemuki/editorservice/EditorServiceApplication.java
+++ src/main/java/com/yvettemuki/editorservice/EditorServiceApplication.java
@@ -1,20 +1,33 @@
 package com.yvettemuki.editorservice;
 
+import net.minidev.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
+@CrossOrigin(origins = "*",allowCredentials = "true",allowedHeaders = "content-type",methods = {})
+@RequestMapping(value = "/api")
 @SpringBootApplication
 public class EditorServiceApplication {
 
-    @CrossOrigin
-    @RequestMapping(value = "/api/test", method = RequestMethod.GET)
-    String home() {
+    @RequestMapping(value = "/test", method = GET)
+    public String home() {
         return "hello world!";
+    }
+
+
+    @RequestMapping(value = "/picture", method = POST)
+    public Object postPicXML(@RequestBody Picture picture) {
+        System.out.println(picture.getXml());
+        System.out.println(picture.getHeight());
+        System.out.println(picture.getWidth());
+        JSONObject json = new JSONObject();
+        json.put("success", true);
+        return json;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yvettemuki/editorservice/Picture.java
@@ -0,0 +1,10 @@
+package com.yvettemuki.editorservice;
+
+import lombok.Data;
+
+@Data
+public class Picture {
+    private String xml;
+    private Integer width;
+    private Integer height;
+}
